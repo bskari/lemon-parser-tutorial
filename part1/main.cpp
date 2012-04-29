ba9115bf8d52342b70c0f46d224f795e6c2d00d1
Brandon Skari <brandon@skari.org>
2012-04-29 16:55:22 -0700

    Added end of tokens symbol

--- part1/main.cpp
+++ part1/main.cpp
@@ -9,14 +9,14 @@ void* ParseFree(void*, void(*freeProc)(void*));
 
 int main()
 {
-  void* shellParser = ParseAlloc(malloc);
-  // Simulate a command line such as "cat main.cpp | wc"
-  Parse(shellParser, FILENAME, "cat");
-  Parse(shellParser, FILENAME, "main.cpp");
-  Parse(shellParser, PIPE, 0);
-  Parse(shellParser, FILENAME, "wc");
-  Parse(shellParser, 0, 0);
-  ParseFree(shellParser, free);
-  return 0;
+    void* shellParser = ParseAlloc(malloc);
+    // Simulate a command line such as "cat main.cpp | wc"
+    Parse(shellParser, FILENAME, "cat");
+    Parse(shellParser, FILENAME, "main.cpp");
+    Parse(shellParser, PIPE, 0);
+    Parse(shellParser, FILENAME, "wc");
+    Parse(shellParser, 0, 0); // Signal end of tokens
+    ParseFree(shellParser, free);
+    return 0;
 }
 
